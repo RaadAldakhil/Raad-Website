b3ceabf0537968f872cd40e4b173df2e83b66c99
Raad <aldakhil95@gmail.com>
2019-09-10 14:16:13 +0300

    updated index and scripts

--- js/scripts.js
+++ js/scripts.js
@@ -27,7 +27,8 @@
 
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        }, 500);
+        // Math.abs(window.pageYOffset - $(heading).offset().top) / 1
 
         // Hide the menu once clicked if mobile
         if ($('header').hasClass('active')) {
@@ -52,6 +53,32 @@
 
     // Create timeline
     $('#experience-timeline').each(function() {
+
+        $this = $(this); // Store reference to this
+        $userContent = $this.children('div'); // user content
+
+        // Create each timeline block
+        $userContent.each(function() {
+            $(this).addClass('vtimeline-content').wrap('<div class="vtimeline-point"><div class="vtimeline-block"></div></div>');
+        });
+
+        // Add icons to each block
+        $this.find('.vtimeline-point').each(function() {
+            $(this).prepend('<div class="vtimeline-icon"><i class="fa fa-map-marker"></i></div>');
+        });
+
+        // Add dates to the timeline if exists
+        $this.find('.vtimeline-content').each(function() {
+            var date = $(this).data('date');
+            if (date) { // Prepend if exists
+                $(this).parent().prepend('<span class="vtimeline-date">'+date+'</span>');
+            }
+        });
+
+    });
+
+    // Create timeline nymber 2
+    $('#experience-timeline-second').each(function() {
 
         $this = $(this); // Store reference to this
         $userContent = $this.children('div'); // user content
