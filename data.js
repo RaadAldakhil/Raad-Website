9e893d0be5afd90505d0f0da3a2598af18e2e4f8
Raad Aldakhil <17354610+RaadAldakhil@users.noreply.github.com>
2020-05-07 17:11:22 +0300

    Revamped Website

---
+++ data.js
@@ -0,0 +1,43 @@
+const main = {
+  name: 'Ra\'ad  Aldakhil',
+  mail: 'aldakhil95@gmail.com',
+  img: './01.jpg',
+  role: ['IT | Dev | Photographer'],
+  // Refer https://ionicons.com/ and put the icon name inside iconName key
+  connects: [
+    {
+      name: 'Github',
+      iconName: 'logo-github',
+      link: 'https://github.com/RaadAldakhil'
+    },
+    {
+      name: 'Linkedin',
+      iconName: 'logo-linkedin',
+      link: 'https://www.linkedin.com/in/RaadAldakhil/'
+    },
+    {
+      name: 'Instagram',
+      iconName: 'logo-instagram',
+      link: 'https://www.instagram.com/raadaldakh/'
+    },
+    {
+      name: 'Twitter',
+      iconName: 'logo-twitter',
+      link: 'https://twitter.com/RaadAldakhil'
+    }, 
+    {
+      name: 'Spotify',
+      iconName: 'musical-notes',
+      link: 'https://open.spotify.com/user/aldakhil95?si=v1BBl4U5Qq2sT3o09UAslA'
+    },
+    {
+      name: 'Mail',
+      iconName: 'mail-outline',
+      link: `mailto:${'aldakhil95@gmail.com'}?Subject=Hello%20again`
+    }
+  ],
+  links: [
+    { name: 'Resume', link: 'RaadAldakhilCV.pdf' },
+    { name: 'Photos', link: 'https://www.instagram.com/raadaldakh/' }
+  ]
+};
