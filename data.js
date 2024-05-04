e099a8eb23996638fa6099d3a05a1b5ec9acd70a
Ra'ad Aldakhil <17354610+RaadAldakhil@users.noreply.github.com>
2024-05-04 20:40:49 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,6 +1,6 @@
 const main = {
   name: 'Ra\'ad  Aldakhil',
-  //mail: 'me@raad.it', If you can find this, then you passed the minimum requirment to email me without hitting the spam filter :) 
+  //mail: 'me@raadaldakhil.com', If you can find this, then you passed the minimum requirment to email me without hitting the spam filter :) 
   img: './01.jpg',
   role: ['Software Engineer'],
   // Refer https://ionicons.com/ and put the icon name inside iconName key
@@ -15,20 +15,20 @@ const main = {
       iconName: 'logo-linkedin',
       link: 'https://www.linkedin.com/in/RaadAldakhil/'
     },
-    {
-      name: 'Twitter',
-      iconName: 'logo-twitter',
-      link: 'https://twitter.com/RaadAldakhil'
-    },
+    //{
+    //  name: 'Twitter',
+    //  iconName: 'logo-twitter',
+    //  link: 'https://twitter.com/RaadAldakhil'
+    //},
     //{
     //  name: 'Instagram',
     //  iconName: 'logo-instagram',
-    //  link: 'https://www.instagram.com/raad.aldakhil/'
+    //  link: 'https://www.instagram.com/raadaldakhil_/'
     //},
   ],
   links: [
-   // { name: 'Blog', link: 'https://blog.raad.it/' },
+   // { name: 'Blog', link: 'https://blog.raadaldakhil.com/' },
     { name: 'Resume', link: 'RaadAldakhilCV.pdf' },
-//    { name: 'Photos', link: 'https://www.instagram.com/raadaldakh/' }
+//    { name: 'Photos', link: 'https://www.instagram.com/raadaldakhil_/' }
   ]
 };
